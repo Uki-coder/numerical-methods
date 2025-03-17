54a989f455400ba61b249a2bc4125fd2c4973515
Andrei Hileuski <pihtatiy.boy@gmail.com>
2025-03-17 22:26:32 +0100

    done lab 3
    universal function for hybryd newton-raphson and bisection methods
    
    checked on examples from tasks

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
+#include <iomanip>
 #include <iostream>
 #include <cmath>
-#include <iomanip>
 
 //Helping functions
 
@@ -23,107 +23,107 @@ double count_error(double x_left, double x_right)
     else return ans;
 }
 
+double middle(double left, double right)
+{
+    return (left + right)/2.;
+}
+
 //Function and its derrivative for task 1
 
 double fun1(double x)
-{
-    return cos(x) - x;
-}
-
-double derrivative_fun1(double x)
-{
-    return -sin(x) - 1;
-}
-
-//Function and its derrivative for task 2
-
-double fun2(double x)
 {
     double ans = (6435. * power(x, 8) - 12012. * power(x, 6)
     + 6930. * power(x, 4) - 1260. * power(x, 2) + 35.)/128.;
     return ans;
 }
 
-double derrivative_fun2(double x)
+double derrivative_fun1(double x)
 {
     double ans = (6435. * 8. * power(x, 7) - 12012. * 6. * power(x, 5)
     + 6930. * 4 * power(x, 3) - 1260. * 2. * x)/128.;
     return ans;
 }
 
-//Function and its derrivative for task 3
+//Function and its derrivative for task 2
 
-double fun3(double x)
+double fun2(double x)
 {
-    return power(x,3) - 169.;
+    return x*x - 2*x -2;
 }
 
-double derrivative_fun3(double x)
+double derrivative_fun2(double x)
 {
-    return 3.*power(x,2);
+    return 2*x - 2.;
 }
 
-//main algorythm 
-
-double count_root(double(*fun)(double), double(*derr)(double),
- double x0, double tolerance = 1e-8)
+//functions for Newton-Raphson step and bisectional step
+double newton_raphson_step(double(*function)(double), double(*derrivative)(double), double x)
 {
-    double x_i, x_i1, error;
-    int counter = 0;
+    return x - function(x)/derrivative(x);
+}
 
-    x_i = x0;
+//main algorythm
+double hybryd_bisec_nr(double x_left, double x0, double x_right,
+     double(*function)(double), double(*derrivative)(double),
+     double tolerance = 1e-8)
+{
+    unsigned int nr_steps, bis_steps;
+    nr_steps = 0;
+    bis_steps = 0;
+    double error = count_error(x_left, x_right);
+    double x_old = x0;
 
-    while (true)
+    while(tolerance < error)
     {
-        x_i1 = x_i - fun(x_i)/derr(x_i);
-        error = count_error(x_i, x_i1);
-        counter++;
-        x_i = x_i1;
+        double derr = derrivative(x0);
+        double check_value = ((x0-x_left)*derrivative(x0) - function(x0))*((x0-x_right)*derrivative(x0) - function(x0)); 
 
-        if (error <= tolerance) break;
+        if(check_value < 0) //NR step
+        {
+            x0 = newton_raphson_step(function, derrivative, x0);
+            nr_steps++;
+        }
+
+        else //bisection step
+        {
+            x0 = middle(x_left, x_right);
+            bis_steps++;
+        }
+
+        if (function(x_left) * function(x0) < 0)
+        {
+            x_right = x0;
+        }
+
+        else
+        {
+            x_left = x0;
+        }
+
+        error = count_error(x0, x_old);
+        x_old = x0;
     }
 
-    return x_i1;
+    std::cout << "Newton-Raphson steps: " << nr_steps << '\n';
+    std::cout << "Bisectional steps: " << bis_steps << "\n\n";
+
+    return x0;
 }
 
-//running function
+
 int main()
 {
+    std::cout << "Task 1" << "\n\n";
+    double root_task1 = hybryd_bisec_nr(0.4, 0.4, 0.7, fun1, derrivative_fun1);
     std::cout << std::setprecision(8);
+    std::cout << root_task1 << '\n';
+    std::cout << "------------------------------------\n\n";
 
-    /*Task 1:
-    cos(x) - x = 0
-    tolerance: 1e-8*/
-
-
-    std::cout << "Task 1: \n";
-    std::cout << "x_root: " << count_root(fun1, derrivative_fun1, 0.) << '\n';
-    // 5 iterations
-    std::cout << "------------------------------\n\n";
-
-    /*
-    Task 2
-    P(x) = (6435x**8 - 12012x**6 + 6930x**4 -1260x**2 +35)/128
-    tolerance: 1e-8
-    */
-
-    std::cout << "Task 2: \n";
-    std::cout << "x_root 1: " << count_root(fun2, derrivative_fun2, 0.15) << '\n';
-    std::cout << "x_root 2: " << count_root(fun2, derrivative_fun2, 0.5) << '\n';
-    std::cout << "x_root 3: " << count_root(fun2, derrivative_fun2, 0.75) << '\n';
-    std::cout << "x_root 4: " << count_root(fun2, derrivative_fun2, 0.9) << '\n';
-    std::cout << "------------------------------\n\n";
-
-    /*
-    Task 3
-    x = 13**(2/3)
-    tolerance: 1e-8
-    */
-
-    std::cout << "Task 3: \n";
-    std::cout << std::setprecision(9) <<"x_root: " << count_root(fun3, derrivative_fun3, 4.) << '\n'; //idk why 9 lol
-    // 5 iterations
-    std::cout << "------------------------------\n\n";
+    std::cout << "Task 2" << "\n\n";
+    double root_task2 = hybryd_bisec_nr(0., 0., 3., fun2, derrivative_fun2);
+    std::cout << std::setprecision(9);
+    std::cout << root_task2 << '\n';
+    std::cout << "------------------------------------\n\n";
 
     return 0;
 }
